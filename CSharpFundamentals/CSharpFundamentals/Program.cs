530a6cca227682ecc60b01380baf8b5dcdbe9a3c
Eray Bakır <info@eraybakir.com>
2020-11-07 14:48:10 +0300

    Commit

--- CSharpFundamentals/CSharpFundamentals/Program.cs
+++ CSharpFundamentals/CSharpFundamentals/Program.cs
@@ -6,7 +6,20 @@ namespace CSharpFundamentals
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Value
+            int number1 = 10;
+            int number2 = 20;
+            number2 = number1; //Değer aktarımı oluğu için number2 number1 e bağlı olarak değişmez
+            number1 = 30;
+            Console.WriteLine(number2);
+
+            //Ref
+            int[] numbers = { 1, 2, 3, 4, 5, 6 };
+            int[] differentNumber = {100,200,300,400,500,600};
+
+            numbers = differentNumber;//Referans aktarımı
+            differentNumber[2] = 700;
+            Console.WriteLine(numbers[2]);
         }
     }
 }
